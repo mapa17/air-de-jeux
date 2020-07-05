2c53549a4b2826a05f00ff32c871595318499800
Manuel Pasieka <manuel.pasieka@protonmail.ch>
2020-07-05 14:06:28 +0200

    Adding color selection, data table and lickage between scatter plot and data table.

--- DS_Service_Challenge/mietspiegel_shiny_viz/ui.R
+++ DS_Service_Challenge/mietspiegel_shiny_viz/ui.R
@@ -1,5 +1,6 @@
 library(shiny)
 library(plotly)
+library(DT)
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
@@ -8,18 +9,26 @@ ui <- fluidPage(
   titlePanel("Mietview"),
   
   # Sidebar with a slider input for number of bins 
-  sidebarLayout(
+  sidebarLayout(position = "right",
     sidebarPanel(
       selectizeInput("xVarSel", 'X-Axis', choices=names(data), selected="mieteqm", multiple=FALSE),
       selectizeInput("yVarSel", 'Y-Axis', choices=names(data), selected="bjahr", multiple=FALSE),
+      selectizeInput("cVarSel", 'Color', choices=names(data), selected="lage", multiple=FALSE),
       hr(),
-      selectizeInput("filterVarSel", 'Filter Variable', choices=names(data), selected="bad", multiple=FALSE),
+      selectizeInput("filterVarSel", 'Filter Variable', choices=names(data), selected="flaeche", multiple=FALSE),
       sliderInput("filterRangeSel", "Range", min = 1,  max = 10, value = c(1, 5)),
     ),
     
     # Show a plot of the generated distribution
     mainPanel(
-      plotlyOutput("distPlot")
+      plotlyOutput("distPlot"),
+      paste0('Filtered Points:'),
+      textOutput("nPoints"),
+      hr(),
+      tags$div(tags$strong("Use the selection tools to filter the scatter points viewed in the table.")),
+      hr(),
+      DT::dataTableOutput("table")
+      
     )
   )
 )
